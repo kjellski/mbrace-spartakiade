f0e68db6f7adc8ff845e82502c8666b8fc880511
Isaac Abraham <isaac.abraham@gmail.com>
2016-03-19 11:46:25 +0100

    Fix thespian.

--- src/FSharp/Scripts/SetupThespian.fsx
+++ src/FSharp/Scripts/SetupThespian.fsx
@@ -1,14 +1,16 @@
 ﻿[<AutoOpen>]
 module SetupThespian
 
+#load @"..\..\..\packages\MBrace.Thespian\MBrace.Thespian.fsx"
+
 // Remember, highlight multiple lines and hit LALT + ENTER to send them to F# Interactive
-#load @"load-references-debug.fsx"
 open System.IO
 open MBrace.Thespian
+
 
 ThespianWorker.LocalExecutable <- Path.Combine(__SOURCE_DIRECTORY__, @"..\..\..\packages\MBrace.Thespian\tools\mbrace.thespian.worker.exe")
 
 let createLocalCluster nodes =
-    ThespianCluster.InitOnCurrentMachine(nodes,
-                                         logger = new ConsoleLogger(), 
-                                         logLevel = LogLevel.Info)
+    ThespianCluster.InitOnCurrentMachine(nodes)
+
+#load @"load-references-debug.fsx"
